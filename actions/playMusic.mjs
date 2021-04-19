76e512c40b79aa349bfb9c158348511965a5828c
FlorianSahbi <florian.sahbi@gmail.com>
2021-04-19 22:40:38 +0200

    Test/Audio quality online

--- actions/playMusic.mjs
+++ actions/playMusic.mjs
@@ -4,6 +4,7 @@ import lookingForSound from "./lookingForSound.mjs";
 import joinVoiceChannel from "./joinVoiceChannel.mjs";
 import UPDATE_GAME_ADD_ROUND from "../graphql/updateGameAddRank.mjs";
 import { request } from 'graphql-request';
+import { differenceInSeconds, format } from "date-fns";
 
 const sendSongMessage = async (message, title, cover) => {
   const songMessage = await message.channel.send({
@@ -37,8 +38,9 @@ const getPoints = (number) => {
 
 }
 
-const addRank = async (game, pos, round) => {
-  const variables = { id: game._id, round: round - 1, position: pos, player: game.players[0]._id, points: getPoints(pos) };
+const addRank = async (game, pos, round, tPoints) => {
+  console.log({ rp: getPoints(pos), tp: tPoints })
+  const variables = { id: game._id, round: round - 1, position: pos, player: game.players[0]._id, points: getPoints(pos) + tPoints };
   return await request(process.env.YU_API, UPDATE_GAME_ADD_ROUND, variables);
 }
 
@@ -46,17 +48,18 @@ const alreadyFindAnswer = (message, finders) => finders.find((u) => finders.incl
 
 
 const playMusic = async (message, song, game, round, pos, finders) => {
-  console.log("DEBUG::playMusic");
+  let newRoundTime = 0;
+  console.log(`DEBUG::playMusic`);
   console.log(`DEBUG::${game._id}`)
   let currentF = finders;
-  // console.log({ input: game });
+  console.log({ input: game });
 
   const connection = await joinVoiceChannel(message);
 
   const dispatcher = await connection
     .play(await ytdl(song.url, { filter: _ => ["251"], highWaterMark: 1 << 25, quality: "highestaudio" }), { type: 'opus' })
     .on("finish", async () => {
-      timer.delete();
+      console.log("END", format(new Date, 'HH:mm:s'))
       await sendSongMessage(message, song.title, song.cover);
       lookingForSound(message, game, round);
     })
@@ -64,8 +67,12 @@ const playMusic = async (message, song, game, round, pos, finders) => {
 
   dispatcher.setVolumeLogarithmic(5 / 5);
   const m = await message.channel.send({ embed: { title: `Round : ${round} - Track is playing 🎶` } });
+  newRoundTime = new Date();
   await sleep(2000)
-  const timer = await message.channel.send("https://thumbs.gfycat.com/FlimsyTemptingBlackfish-size_restricted.gif");
+  const timer = await message.channel.send("https://thumbs.gfycat.com/FlimsyTemptingBlackfish-size_restricted.gif").then(msg => {
+    msg.delete({ timeout: 10000 /*time unitl delete in milliseconds*/ });
+  })
+    .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);;
 
   const filter = m => !m.author.bot;
 
@@ -75,7 +82,11 @@ const playMusic = async (message, song, game, round, pos, finders) => {
     if (song.correctWords.includes(m.content) && !alreadyFindAnswer(message, finders)) {
       m.delete()
       m.reply(`got it`);
-      const { updateGameAddRank } = await addRank(game, pos, round);
+      const now = new Date();
+      console.log("1", differenceInSeconds(now, newRoundTime))
+      console.log("2", differenceInSeconds(now, newRoundTime) - (game.trackTime / 1000))
+      console.log("3", -(differenceInSeconds(now, newRoundTime) - (game.trackTime / 1000)))
+      const { updateGameAddRank } = await addRank(game, pos, round, -(differenceInSeconds(now, newRoundTime) - (game.trackTime / 1000)));
       pos = pos + 1;
       currentF.push(message.author.id)
     } else if (song.correctWords.includes(m.content) && alreadyFindAnswer(message, finders)) {
--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 import { Client } from "discord.js";
 import initGame from "./actions/initGame.mjs";
+import ytdl from "ytdl-core-discord";
+import { format } from "date-fns";
 
 const client = new Client();
 const config = { prefix: "!" };
 
 client.once("ready", () => {
-  console.log(`${Date.now()} : Ready`)
+  console.log(`${format(new Date(), 'HH:mm:s')} : Ready`)
 });
 
 client.once("reconnecting", () => {
@@ -25,6 +27,32 @@ client.on("message", async message => {
     initGame(message)
     return;
   }
+  else if (message.content.startsWith(`${config.prefix}test`)) {
+    if (!message.member.voice.channel) {
+      message.channel.send("You need to be in a voice channel to play music!");
+    }
+    const permissions = message.member.voice.channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      message.channel.send("I need the permissions to join and speak in your voice channel!");
+    }
+    const videoID = "https://www.youtube.com/watch?v=rTJjtSP-ORc"
+    // const videoID = "https://www.youtube.com/watch?v=Ox2ySH4EAc8"
+    const connection = await message.member.voice.channel.join();
+    let info = await ytdl.getInfo(videoID);
+    let audioFormats = await ytdl.filterFormats(info.formats, 'audioonly');
+    let format = ytdl.chooseFormat(info.formats, { quality: '251' });
+    // await connection.play(await ytdl(videoID, {quality:"highest",filter:"audioandvideo"}), { type: 'opus' })
+    await connection.play(await ytdl(videoID, {highWaterMark: 50, quality:"highest",filter:"audioonly"}), { type: 'opus' })
+    console.log('Format found!', format);
+
+    // const dispatcher = await connection
+    //   .play(await ytdl(videoID, { highWaterMark: 50 }), { type: 'opus' })
+    //   .on("finish", async () => {})
+    //   .on("info", async (info) => console.log(info))
+    //   .on("error", error => console.error(error));
+    return;
+  }
+
   else {
     message.channel.send("You need to enter a valid command!");
   }
